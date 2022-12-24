3d7cf93d9732d56d6f08328628b35b6d1d998572
luciano <lucianosoaresdev748@gmail.com>
2022-12-24 18:25:02 -0300

    comit first NavBar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import NavBar from "./components/NavBar";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 function App() {
   return (
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,49 @@
 import React from "react";
-// import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars } from "react-icons/fa";
 
 const NavBar = () => {
+  const links = [
+    {
+      id: 1,
+      link: "home",
+    },
+    {
+      id: 2,
+      link: "about",
+    },
+    {
+      id: 3,
+      link: "portfolio",
+    },
+    {
+      id: 4,
+      link: "experience",
+    },
+    {
+      id: 5,
+      link: "contact",
+    },
+  ];
+
   return (
-    <div class="flex justify-between items-center w-full h-20 px-4 text-white bg-black ">
+    <div className="flex justify-between items-center w-full h-20 text-white px-4 bg-black fixed">
       <div>
-        <h1>Luciano</h1>
+        <h1 className="font-signature ml-2 text-5xl">Luciano</h1>
+      </div>
+
+      <ul className="hidden md:flex ">
+        {links.map(({ id, link }) => (
+          <li
+            key={link.id}
+            className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
+          >
+            {link}
+          </li>
+        ))}
+      </ul>
+
+      <div className="cursor-pointer pr-4 z-10 text-gray-500">
+        <FaBars />
       </div>
     </div>
   );
