5e36b2c1e728097f0a9fd8b0a6ef98e5b0b13d5b
luciano <lucianosoaresdev748@gmail.com>
2022-12-24 19:47:57 -0300

    init create Home

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import HeroImage from "../assets/heroImage.png";
+
+export const Home = () => {
+  return (
+    <div>
+      <img src={HeroImage} alt="" />
+    </div>
+  );
+};
