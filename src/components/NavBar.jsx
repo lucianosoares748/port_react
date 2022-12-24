7534c8a22b93628204d7f2b1138dae0f5d2d27f7
luciano <lucianosoaresdev748@gmail.com>
2022-12-24 16:28:42 -0300

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import { FaBars, FaTimes } from "react-icons/fa";
+
+const NavBar = () => {
+  return (
+    <div class="flex justify-between items-center w-full h-20 px-4 text-white bg-black ">
+      <div>
+        <h1>Luciano</h1>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
