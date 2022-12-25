388ad3e364515dd12622dceff15245d4ee276537
luciano <lucianosoaresdev748@gmail.com>
2022-12-25 12:56:24 -0300

    structure home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,42 @@
 import React from "react";
 import HeroImage from "../assets/heroImage.png";
-
+import { MdKeyboardArrowRight } from "react-icons/md";
 export const Home = () => {
   return (
-    <div>
-      <img src={HeroImage} alt="" />
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">
+            I' am a front-end developer
+          </h2>
+          <p className="text-gray-500 py-4 max-w-md">
+            I've been studying web development for a year now, in the front-end
+            developer area. Currently, I love working on web applications using
+            technologies like React, Tailwind, Next.JS, JavaScript and
+            typescript.
+          </p>
+
+          <div>
+            <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer ">
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300">
+                <MdKeyboardArrowRight size={25} className="ml-1" />
+              </span>
+            </button>
+          </div>
+        </div>
+
+        <div>
+          <img
+            src={HeroImage}
+            alt="my profile"
+            className="rounded-2xl mx-auto w-2/3 md:w-full"
+          />
+        </div>
+      </div>
     </div>
   );
 };
