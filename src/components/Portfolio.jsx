440105232ca756df2488b816b9ecf3ce3a24ccac
luciano <lucianosoaresdev748@gmail.com>
2022-12-26 17:08:30 -0300

    deleted images

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,37 +1,13 @@
 import React from "react";
-import arrayDestruct from "../assets/portfolio/arrayDestruct.jpg";
-import installNode from "../assets/portfolio/installNode.jpg";
-import navbar from "../assets/portfolio/navbar.jpg";
-import reactParallax from "../assets/portfolio/reactParallax.jpg";
-import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
-import reactWeather from "../assets/portfolio/reactWeather.jpg";
+import { proHome } from "../assets/portfolio/proHome.png";
 
 const Portfolio = () => {
   const portfolios = [
     {
       id: 1,
-      src: arrayDestruct,
+      src: proHome,
       url: "https://darling-frangipane-42c5a1.netlify.app/",
-    },
-    {
-      id: 2,
-      src: reactParallax,
-    },
-    {
-      id: 3,
-      src: navbar,
-    },
-    {
-      id: 4,
-      src: reactSmooth,
-    },
-    {
-      id: 5,
-      src: installNode,
-    },
-    {
-      id: 6,
-      src: reactWeather,
+      url2: "https://github.com/lucianosoares748/home",
     },
   ];
 
@@ -64,7 +40,7 @@ const Portfolio = () => {
                   <a href={portfolio?.url}>Demo</a>
                 </button>
                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  Code
+                  <a href={portfolio?.url2}>Code</a>
                 </button>
               </div>
             </div>
