d067a5dbf2e8b749731779d757ff22eefdb9119f
luciano <lucianosoaresdev748@gmail.com>
2022-12-26 11:32:18 -0300

    responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -29,7 +29,7 @@ const NavBar = () => {
   ];
 
   return (
-    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
+    <div className="flex justify-between items-center w-full h-20 sticky top-0 px-4 text-white bg-black fixed">
       <div>
         <h1 className="text-5xl font-signature ml-2">Luciano</h1>
       </div>
