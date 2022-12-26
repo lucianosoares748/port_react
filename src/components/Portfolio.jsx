4098d294371afad9bc27ea13c10cc9508bbd4e67
luciano <lucianosoaresdev748@gmail.com>
2022-12-26 16:43:40 -0300

    url button

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -57,11 +57,8 @@ const Portfolio = () => {
                 className="rounded-md duration-200 hover:scale-105"
               />
               <div className="flex items-center justify-center">
-                <button
-                  to={url}
-                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
-                >
-                  Demo
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  <a href={url}>Demo</a>
                 </button>
                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                   Code
