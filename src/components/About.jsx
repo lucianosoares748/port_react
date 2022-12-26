2f5ff62ded9c76ee0e9ec63925a526e1f9c14810
luciano <lucianosoaresdev748@gmail.com>
2022-12-26 10:45:56 -0300

    mobile responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,12 +7,12 @@ const About = () => {
       className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-            About
-          </p>
-        </div>
         <p className="text-xl mt-20 ">
+          <div className="pb-8">
+            <p className="text-4xl mt-2000 font-bold inline border-b-4 border-gray-500">
+              About
+            </p>
+          </div>
           Olá eu chamo Luciano dos Santos Soares, tenho 25 anos e moro
           atualmente em Bataguassu-MS, desde muito pequeno sempre tive
           curiosidade, e interesse pela areá da tecnologia, para jogos, e
