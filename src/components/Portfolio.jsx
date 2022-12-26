e288c8c63c18489a17736f84de80a410fa2996a6
luciano <lucianosoaresdev748@gmail.com>
2022-12-26 16:50:48 -0300

    start

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -49,16 +49,19 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src, url }) => (
-            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+          {portfolios.map((portfolio) => (
+            <div
+              key={portfolio.id}
+              className="shadow-md shadow-gray-600 rounded-lg"
+            >
               <img
-                src={src}
+                src={portfolio.src}
                 alt="imagem do perfil do projeto"
                 className="rounded-md duration-200 hover:scale-105"
               />
               <div className="flex items-center justify-center">
                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  <a href={url}>Demo</a>
+                  <a href={portfolio?.url}>Demo</a>
                 </button>
                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                   Code
